42e75e4066284641e7ae3689cf6f1a4c05b73ca7
Brian Christopher Mendoza <agi2400@CORP.JTI.COM>
2024-09-19 17:52:41 +0800

    [Feature] API Integration for Footer and Login Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import { Box, Typography } from "@mui/material";
+"use client"
+
+import { Container } from "@/app/_components/layout/Container"
+import { Text } from "@/app/_components/ui/Text";
+import { Footer } from "@/app/_components/sections/Footer";
+import { LoginForm } from "@/app/_components/forms/LoginForm";
+import { TextField, Button } from "@mui/material";
+
+import { usePublicConfig } from "./hooks/usePublicConfig";
+
+type TConfigValues = {
+  config_items: {
+    [key: string]: string
+  },
+  menu_items: {
+    [key: string]: string
+  }
+}
+
+type TConfigState = {
+  data?: TConfigValues;
+  isLoading: boolean;
+  error?: unknown;
+}
 
 export default function Home() {
+  const config: TConfigState = usePublicConfig()
+
   return (
-    <Box component="main">
-      <Typography component="h1">Hello with Vitest</Typography>
-    </Box>
+    <Container component="main" sx={{ height: "100vh", bgcolor: "#FDE600", flexDirection: "column", padding: 0 }}>
+      <Text component="h1" sx={{ color: "#1D71A4", fontWeight: "900" }}>Log-in</Text>
+      <LoginForm>
+        <TextField
+          name="username"
+          label="username"
+          />
+        <TextField
+          name="password"
+          label="password"
+          />
+        <Button type="submit">Submit</Button>
+      </LoginForm>
+      {config.isLoading ? <Text>Loading...</Text> : (
+        <Footer config_items={config.data?.config_items ?? {}} />
+      )}
+    </Container>
   );
 }
