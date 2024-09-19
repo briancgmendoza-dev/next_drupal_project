a30b20b54ca33ea5f8a17d461f5817f8049a6f3b
Brian Christopher Mendoza <agi2400@CORP.JTI.COM>
2024-09-19 17:52:07 +0800

    [Feature] Implementing Authenticate Service

---
+++ app/services/public/authenticate.ts
@@ -0,0 +1,28 @@
+import request from "@/app/services/request";
+import { BASE_URL } from "@/app/utils/constants";
+
+type TAuthPayload = {
+  formData: FormData
+}
+
+export const AuthenticateService = {
+  getToken: async (signal: AbortSignal, payload: TAuthPayload) => {
+    try {
+      const response = await request({
+        url: `${BASE_URL}oauth/token`,
+        method: "POST",
+        payload: payload.formData,
+        signal
+      })
+      if (response.data.access_token) {
+        return { isAuthenticated: true }
+      }
+      return { isAuthenticated: false }
+    } catch (error: unknown) {
+      if (error instanceof Error && error.name === "AbortError") {
+        console.log("Aborted Duplicate Request")
+      }
+      return { error }
+    }
+  }
+}
